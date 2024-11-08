c7f637833495a334313abda0e1f369f75990850c
Vladislav Bezushko <bezushkovladis@gmail.com>
2024-11-08 19:21:30 +0200

    Solution1

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,6 +5,43 @@
  */
 function makeCalculator() {
   // write code here
+  let result = 0;
+
+  return {
+    get result() {
+      return result;
+    },
+    add(num) {
+      result += num;
+
+      return this;
+    },
+    subtract(num) {
+      result -= num;
+
+      return this;
+    },
+    multiply(num) {
+      result *= num;
+
+      return this;
+    },
+    divide(num) {
+      result /= num;
+
+      return this;
+    },
+    operate(callback, num) {
+      callback(num);
+
+      return this;
+    },
+    reset() {
+      result = 0;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
